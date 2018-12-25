bb1d53843b1b213f31dd6bec4adb726456993ae3
FREEMAN <454592297@qq.com>
2018-12-25 15:18:28 +0800

    1

--- nio/TestBuffer.java
+++ nio/TestBuffer.java
@@ -28,10 +28,6 @@ import org.junit.Test;
  * 四、直接缓冲区与非直接缓冲区：
  * 非直接缓冲区：通过 allocate() 方法分配缓冲区，将缓冲区建立在 JVM 的内存中
  * 直接缓冲区：通过 allocateDirect() 方法分配直接缓冲区，将缓冲区建立在物理内存中。可以提高效率
- * 
- * 五、直接缓冲区和非直接缓冲区
- * 非直接缓冲区：通过allocate()方法分配缓冲区，将缓冲区建立在JVM的内存中
- * 直接缓冲区：通过allocateDirect()方法分配直接缓冲区，将缓冲区建立在物理内存中
  */
 
 public class TestBuffer {
